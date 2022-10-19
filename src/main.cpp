e69099a58c9c48701efc75b04ec0f89521a1ae80
michaelskyf <michal.kostrzewski@protonmail.com>
2022-10-19 16:06:10 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+Program 13 - Huffman
+Napisać program do kompresji plików metodą Huffmanna.
+Program uruchamiany jest z linii poleceń z wykorzystaniem następujących przełączników:
+-i plik wejściowy
+-o plik wyjściowy
+-t tryb: k – kompresja, d – dekompresja
+-s plik ze słownikiem (tworzonym w czasie kompresji, używanym w czasie
+dekompresji)
+*/
